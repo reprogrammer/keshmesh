7e002361d2ba58d660d97833b5e2274f5ed909c5
Mohsen Vakilian <reprogrammer@gmail.com>
2011-01-05 15:49:56 -0600

    Switched back to receive instance context and selected a single entry point. The selected entry point reaches the instance of LCK02J in Tomcat.

--- edu.illinois.keshmesh.detector/src/edu/illinois/keshmesh/detector/EclipseProjectAnalysisEngine.java
+++ edu.illinois.keshmesh.detector/src/edu/illinois/keshmesh/detector/EclipseProjectAnalysisEngine.java
@@ -5,10 +5,14 @@ package edu.illinois.keshmesh.detector;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.IJavaProject;
 
+import com.ibm.wala.classLoader.IClass;
+import com.ibm.wala.classLoader.IMethod;
 import com.ibm.wala.client.AbstractAnalysisEngine;
 import com.ibm.wala.ide.util.EclipseProjectPath;
 import com.ibm.wala.ide.util.EclipseProjectPath.AnalysisScopeType;
@@ -19,8 +23,15 @@ import com.ibm.wala.ipa.callgraph.CallGraphBuilder;
 import com.ibm.wala.ipa.callgraph.ContextSelector;
 import com.ibm.wala.ipa.callgraph.Entrypoint;
 import com.ibm.wala.ipa.callgraph.impl.ContextInsensitiveSelector;
+import com.ibm.wala.ipa.callgraph.impl.DefaultEntrypoint;
 import com.ibm.wala.ipa.callgraph.impl.Util;
 import com.ibm.wala.ipa.cha.IClassHierarchy;
+import com.ibm.wala.types.ClassLoaderReference;
+import com.ibm.wala.types.Descriptor;
+import com.ibm.wala.types.MethodReference;
+import com.ibm.wala.types.TypeReference;
+import com.ibm.wala.util.collections.HashSetFactory;
+import com.ibm.wala.util.strings.Atom;
 
 /**
  * 
@@ -49,9 +60,9 @@ public class EclipseProjectAnalysisEngine extends AbstractAnalysisEngine {
 	@Override
 	protected CallGraphBuilder getCallGraphBuilder(IClassHierarchy classHierarchy, AnalysisOptions analysisOptions, AnalysisCache analysisCache) {
 		//		ContextSelector contextSelector = new CustomReceiverInstanceContextSelector();
-		ContextSelector contextSelector = new ContextInsensitiveSelector();
+//		ContextSelector contextSelector = new ContextInsensitiveSelector();
 		//		ContextSelector contextSelector = new ReceiverTypeContextSelector();
-		//		ContextSelector contextSelector = new CustomReceiverTypeContextSelector();
+				ContextSelector contextSelector = new CustomReceiverTypeContextSelector();
 		Util.addDefaultSelectors(analysisOptions, classHierarchy);
 		Util.addDefaultBypassLogic(analysisOptions, scope, Util.class.getClassLoader(), classHierarchy);
 		return new KeshmeshCFABuilder(classHierarchy, analysisOptions, analysisCache, contextSelector, null);
@@ -59,8 +70,51 @@ public class EclipseProjectAnalysisEngine extends AbstractAnalysisEngine {
 
 	@Override
 	protected Iterable<Entrypoint> makeDefaultEntrypoints(AnalysisScope analysisScope, IClassHierarchy classHierarchy) {
-		Iterable<Entrypoint> mainEntrypoints = Util.makeMainEntrypoints(analysisScope.getApplicationLoader(), classHierarchy);
-		return mainEntrypoints;
+		//Iterable<Entrypoint> mainEntrypoints = Util.makeMainEntrypoints(analysisScope.getApplicationLoader(), classHierarchy);
+		//		return new FilteredIterable(mainEntrypoints);
+		return makeCustomEntryPointes(analysisScope, classHierarchy);
+	}
+
+	private Iterable<Entrypoint> makeCustomEntryPointes(AnalysisScope analysisScope, IClassHierarchy classHierarchy) {
+		//org.apache.catalina.startup.TestTomcat.testEnableNamingGlobal
+		final HashSet<Entrypoint> result = HashSetFactory.make();
+		ClassLoaderReference applicationLoader = analysisScope.getApplicationLoader();
+		TypeReference declaringClassReference = TypeReference.findOrCreateClass(applicationLoader, "org/apache/catalina/startup", "TestTomcat");
+		Atom testMethodName = Atom.findOrCreateAsciiAtom("testEnableNamingGlobal");
+		MethodReference testMehodReference = MethodReference.findOrCreate(declaringClassReference, testMethodName, Descriptor.findOrCreateUTF8("()V"));
+		IClass declaringClass = classHierarchy.lookupClass(declaringClassReference);
+		IMethod testMethod = declaringClass.getMethod(testMehodReference.getSelector());
+		System.out.println("testMethod:" + testMethod.getSignature());
+		result.add(new DefaultEntrypoint(testMethod, classHierarchy));
+
+		return new Iterable<Entrypoint>() {
+			public Iterator<Entrypoint> iterator() {
+				return result.iterator();
+			}
+		};
+	}
+
+	static class FilteredIterable implements Iterable<Entrypoint> {
+
+		Iterable<Entrypoint> originalIterable;
+
+		public FilteredIterable(Iterable<Entrypoint> originalIterable) {
+			super();
+			this.originalIterable = originalIterable;
+		}
+
+		@Override
+		public Iterator<Entrypoint> iterator() {
+			final HashSet<Entrypoint> filteredEntrypoints = HashSetFactory.make();
+			Iterator<Entrypoint> originalItarator = originalIterable.iterator();
+			while (originalItarator.hasNext()) {
+				Entrypoint nextEntrypoint = originalItarator.next();
+				if (nextEntrypoint.getMethod().getDeclaringClass().getName().getPackage().toString().contains("org/apache/catalina/startup/Bootstrap")) {
+					filteredEntrypoints.add(nextEntrypoint);
+				}
+			}
+			return filteredEntrypoints.iterator();
+		}
 	}
 
 }
