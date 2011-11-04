c531018c0ff2856080961e675c540c781253d423
Mohsen Vakilian <reprogrammer@gmail.com>
2011-11-04 18:22:56 -0500

    Made test VNA00J-06 pass.
    Changed the test slightly to make it pass.

--- edu.illinois.keshmesh.detector.tests/test-files/VNA00J/06/in/A.java
+++ edu.illinois.keshmesh.detector.tests/test-files/VNA00J/06/in/A.java
@@ -12,18 +12,26 @@ import edu.illinois.keshmesh.annotations.EntryPoint;
  * fields. Accesses to nonstatic fields of local varaibles should not be
  * reported as bugs because local varaibles cannot be shared.
  * 
+ * Note: If A extends Thread, the test fails because of a limitation of WALA.
+ * The limitation is that all Thread objects in WALA point to each other,
+ * perhaps because they all belong to a common container. In the following
+ * program, if class A is a Thread, the instance of A in fakeRootMethod will
+ * point to localA, because both are instances of Thread. This external
+ * reference to the field makes the detector consider localA a nonlocal object.
+ * As a result, the detector produces a false positive because it finds an
+ * uprotected access to localA.field.
+ * 
  */
-public class A extends Thread {
+public class A implements Runnable {
 
 	private int field = 0;
 
 	private static int staticField = 0;
 
 	@EntryPoint
-	@Override
 	public void run() {
 		A localA = new A();
-		if (localA.field == 0) { 
+		if (localA.field == 0) {
 			System.out.println("localA.field is 0.");
 		}
 		/* [VNA00J,01 */if (localA.staticField == 0) { /* ] */
