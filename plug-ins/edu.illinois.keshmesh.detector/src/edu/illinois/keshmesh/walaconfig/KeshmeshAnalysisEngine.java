7f6d1c1c87078896e6c531bcfe71cde6f077fdbd
Mohsen Vakilian <reprogrammer@gmail.com>
2013-11-25 15:49:16 -0600

    Removed unused code from class KeshmeshAnalysisEngine.

--- plug-ins/edu.illinois.keshmesh.detector/src/edu/illinois/keshmesh/walaconfig/KeshmeshAnalysisEngine.java
+++ plug-ins/edu.illinois.keshmesh.detector/src/edu/illinois/keshmesh/walaconfig/KeshmeshAnalysisEngine.java
@@ -6,7 +6,6 @@ package edu.illinois.keshmesh.walaconfig;
 import java.util.HashSet;
 import java.util.Iterator;
 
-import com.ibm.wala.cast.java.ipa.callgraph.JavaSourceAnalysisScope;
 import com.ibm.wala.classLoader.IClass;
 import com.ibm.wala.classLoader.IMethod;
 import com.ibm.wala.classLoader.ShrikeCTMethod;
@@ -23,13 +22,9 @@ import com.ibm.wala.ipa.callgraph.propagation.cfa.ZeroXCFABuilder;
 import com.ibm.wala.ipa.callgraph.propagation.cfa.ZeroXInstanceKeys;
 import com.ibm.wala.ipa.cha.IClassHierarchy;
 import com.ibm.wala.types.ClassLoaderReference;
-import com.ibm.wala.types.Descriptor;
-import com.ibm.wala.types.MethodReference;
 import com.ibm.wala.types.TypeName;
-import com.ibm.wala.types.TypeReference;
 import com.ibm.wala.types.annotations.Annotation;
 import com.ibm.wala.util.collections.HashSetFactory;
-import com.ibm.wala.util.strings.Atom;
 
 import edu.illinois.keshmesh.annotations.EntryPoint;
 import edu.illinois.keshmesh.detector.util.AnalysisUtils;
@@ -106,87 +101,5 @@ public class KeshmeshAnalysisEngine {
 	private static boolean isEntryPointClass(TypeName typeName) {
 		return (AnalysisUtils.walaTypeNameToJavaName(typeName).equals(EntryPoint.class.getName()));
 	}
-
-	public static Iterable<Entrypoint> makeCustomEntryPoints(AnalysisScope analysisScope, IClassHierarchy classHierarchy) {
-		//org.apache.catalina.startup.TestTomcat.testEnableNamingGlobal
-		final HashSet<Entrypoint> result = HashSetFactory.make();
-		ClassLoaderReference loaderReference = JavaSourceAnalysisScope.SOURCE;
-		//		TypeReference declaringClassReference = TypeReference.findOrCreateClass(applicationLoader, "org/apache/catalina/startup", "TestTomcat");
-		//		TypeReference declaringClassReference = TypeReference.findOrCreateClass(applicationLoader, "javax/servlet/http", "HttpServlet");
-		TypeReference declaringClassReference = TypeReference.findOrCreateClass(loaderReference, "org/apache/tomcat/lite/io", "SocketConnector");
-		//		TypeReference declaringClassReference = TypeReference.findOrCreateClass(applicationLoader, "org/apache/naming/java", "javaURLContextFactory");
-		//		Atom testMethodName = Atom.findOrCreateAsciiAtom("service");
-		//		MethodReference testMehodReference = MethodReference.findOrCreate(declaringClassReference, testMethodName, Descriptor.findOrCreateUTF8("(Ljavax/servlet/ServletRequest;Ljavax/servlet/ServletResponse;)V"));
-		Atom testMethodName = Atom.findOrCreateAsciiAtom("getSelector");
-		MethodReference testMehodReference = MethodReference.findOrCreate(declaringClassReference, testMethodName, Descriptor.findOrCreateUTF8("()Lorg/apache/tomcat/lite/io/NioThread;"));
-		//		Atom testMethodName = Atom.findOrCreateAsciiAtom("getInitialContext");
-		//		MethodReference testMehodReference = MethodReference.findOrCreate(declaringClassReference, testMethodName, Descriptor.findOrCreateUTF8("(Ljava/util/Hashtable;)Ljavax/naming/Context;"));
-		IClass declaringClass = classHierarchy.lookupClass(declaringClassReference);
-		//		for (IMethod method : declaringClass.getAllMethods()){
-		//			System.out.println("Signature:" + method.getSignature());
-		//		}
-		IMethod testMethod = declaringClass.getMethod(testMehodReference.getSelector());
-		System.out.println("testMethod:" + testMethod.getSignature());
-		result.add(new DefaultEntrypoint(testMethod, classHierarchy));
-
-		return new Iterable<Entrypoint>() {
-			public Iterator<Entrypoint> iterator() {
-				return result.iterator();
-			}
-		};
-	}
-
-	//	public static Iterable<Entrypoint> makeCustomEntryPoints(AnalysisScope analysisScope, IClassHierarchy classHierarchy) {
-	//		//org.apache.catalina.startup.TestTomcat.testEnableNamingGlobal
-	//		final HashSet<Entrypoint> result = HashSetFactory.make();
-	//		ClassLoaderReference applicationLoader = analysisScope.getApplicationLoader();
-	//		//		TypeReference declaringClassReference = TypeReference.findOrCreateClass(applicationLoader, "org/apache/catalina/startup", "TestTomcat");
-	//		//		TypeReference declaringClassReference = TypeReference.findOrCreateClass(applicationLoader, "javax/servlet/http", "HttpServlet");
-	//		TypeReference declaringClassReference = TypeReference.findOrCreateClass(applicationLoader, "org/apache/catalina/startup", "TestTomcat$HelloWorldJndi");
-	//		//		TypeReference declaringClassReference = TypeReference.findOrCreateClass(applicationLoader, "org/apache/naming/java", "javaURLContextFactory");
-	//		//		Atom testMethodName = Atom.findOrCreateAsciiAtom("service");
-	//		//		MethodReference testMehodReference = MethodReference.findOrCreate(declaringClassReference, testMethodName, Descriptor.findOrCreateUTF8("(Ljavax/servlet/ServletRequest;Ljavax/servlet/ServletResponse;)V"));
-	//		Atom testMethodName = Atom.findOrCreateAsciiAtom("doGet");
-	//		MethodReference testMehodReference = MethodReference.findOrCreate(declaringClassReference, testMethodName,
-	//				Descriptor.findOrCreateUTF8("(Ljavax/servlet/http/HttpServletRequest;Ljavax/servlet/http/HttpServletResponse;)V"));
-	//		//		Atom testMethodName = Atom.findOrCreateAsciiAtom("getInitialContext");
-	//		//		MethodReference testMehodReference = MethodReference.findOrCreate(declaringClassReference, testMethodName, Descriptor.findOrCreateUTF8("(Ljava/util/Hashtable;)Ljavax/naming/Context;"));
-	//		IClass declaringClass = classHierarchy.lookupClass(declaringClassReference);
-	//		//		for (IMethod method : declaringClass.getAllMethods()){
-	//		//			System.out.println("Signature:" + method.getSignature());
-	//		//		}
-	//		IMethod testMethod = declaringClass.getMethod(testMehodReference.getSelector());
-	//		System.out.println("testMethod:" + testMethod.getSignature());
-	//		result.add(new DefaultEntrypoint(testMethod, classHierarchy));
-	//
-	//		return new Iterable<Entrypoint>() {
-	//			public Iterator<Entrypoint> iterator() {
-	//				return result.iterator();
-	//			}
-	//		};
-	//	}
-	//
-	//	static class FilteredIterable implements Iterable<Entrypoint> {
-	//
-	//		Iterable<Entrypoint> originalIterable;
-	//
-	//		public FilteredIterable(Iterable<Entrypoint> originalIterable) {
-	//			super();
-	//			this.originalIterable = originalIterable;
-	//		}
-	//
-	//		@Override
-	//		public Iterator<Entrypoint> iterator() {
-	//			final HashSet<Entrypoint> filteredEntrypoints = HashSetFactory.make();
-	//			Iterator<Entrypoint> originalItarator = originalIterable.iterator();
-	//			while (originalItarator.hasNext()) {
-	//				Entrypoint nextEntrypoint = originalItarator.next();
-	//				if (nextEntrypoint.getMethod().getDeclaringClass().getName().getPackage().toString().contains("org/apache/catalina/startup/Bootstrap")) {
-	//					filteredEntrypoints.add(nextEntrypoint);
-	//				}
-	//			}
-	//			return filteredEntrypoints.iterator();
-	//		}
-	//	}
 
 }
